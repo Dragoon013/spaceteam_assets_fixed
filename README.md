f02232d8b0b225c44072ace70457e8b2bfc9c58d
Erica Lauer Vose <elauervose@gmail.com>
2015-10-13 15:10:34 -0700

    change performance issues to highlight NR browser features

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # NRU_Space_Team
 This is the NRU Team's Space App for Browser
+
+# Normal
+cap production deploy
+
+# Changes not showing up?
+cap production puma:restart
+
+# Getting a 502?
+## Connect to the Amazon server
+ssh -i ~/.ssh/NRUTeamErica.pem ubuntu@52.88.223.59
+
+## Switch to the deploy user
+su - deploy
+
+## Restart Puma
+cd /home/deploy/nru-space-team/current && ( RAILS_ENV=production ~/.rvm/bin/rvm ruby-2.2.3 do bundle exec puma -b 'unix:///home/deploy/nru-space-team/shared/tmp/sockets/puma.sock'  -e production  --control 'unix:///home/deploy/nru-space-team/shared/tmp/sockets/pumactl.sock' -S /home/deploy/nru-space-team/shared/tmp/pids/puma.state  >> /home/deploy/nru-space-team/shared/log/puma-production.log 2>&1 & )
+
+## Check that puma is running
+ps ax | grep puma
+[Look for 'puma: cluster worker 0: xxxxx [date]']
+
+## exit
+exit
+exit
---
+++ app/assets/javascripts/space-errors.js
@@ -0,0 +1,102 @@
+function randomNumber1() {
+      var x = Math.floor((Math.random() * 1000) + 1);
+      document.getElementById("number1").innerHTML = x;
+  };
+  function randomNumber2() {
+      var x = Math.floor((Math.random() * 500));
+      document.getElementById("number2").innerHTML = x;
+  };
+  function randomNumber3() {
+      var x = Math.floor((Math.random() * 200) + 1);
+      document.getElementById("number3").innerHTML = x;
+
+  };
+
+  function bp() {
+    jQuery('#green').hide();
+    jQuery('#red').hide();
+    r = Math.random() * 2.0;
+    jQuery.ajax("/errors/ajax?p=" + r, {
+      success: function (data,status) {
+        jQuery('#green').show();
+      },
+      complete: function (xhr,status) {
+        if (status != "success") {
+          jQuery('#red').show();
+        }
+      }
+    });
+  }
+  jQuery().ready(function () {
+    jQuery('#green').hide();
+    jQuery('#red').hide();
+  });
+
+  function gridSearch() {
+    jQuery('#green').hide();
+    jQuery('#red').hide();
+  }
+
+function toggle() {
+  $("#search").click(function() {
+      var sourceElement = $('#source-div'); //Source Div Element
+      var destinationElement = $('#dest-div'); //Destination Div element
+      var contentToMove = $('#content-div'); //Content Div Element to move
+      var SourceDivEmpty = sourceElement.is(":empty"); //returns true if element is empty
+
+      if(SourceDivEmpty) //return true if source element is empty
+      {
+          contentToMove.appendTo(sourceElement);
+          destinationElement.empty();
+      }else{
+          contentToMove.appendTo(destinationElement);
+          sourceElement.empty();
+      }
+  });
+}
+$(document).ready(function() {
+    toggle()
+});
+
+
+function openChannel() {
+    alert("Channel open, set hailing frequency");
+};
+function hailingFrequency() {
+    alert("Hailing frequency set");
+};
+
+
+$(document).ready( function(){
+  randomNumber1();
+  randomNumber2();
+  randomNumber3();
+  console.log("Red Alert");
+  bp();
+  toggle();
+  gridSearch();
+  systemWarning();
+  probeLaunchSequence('Begin Launch Sequence for Class IV Probe');
+});
+
+// Move this
+
+  function systemsCheck() {
+    var #search1 = $("#search1").click(function() {
+        onclick.("#search1").hide();
+        onclick.("#search2").show();
+
+
+        if("#search2") //return true if source element is empty
+        {
+
+        }else{
+            onclick.("#search1").show();
+            onclick.("#search2").hide();
+        }
+    });
+  }
+
+  $(document).ready( function(){
+    systemsCheck();
+  });
